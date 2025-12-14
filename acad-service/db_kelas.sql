2cce6807cb7ca568b8c49253e30adbb9596128d2
upi <wajibro@yahoo.com>
2025-12-14 21:30:32 +0700

    Kostumisasi data mahasiswa

--- acad-service/db_kelas.sql
+++ acad-service/db_kelas.sql
@@ -26,24 +26,40 @@ CREATE TABLE bobot_nilai (
     bobot FLOAT
 );
 
-
 INSERT INTO mahasiswa (nim, nama, jurusan, angkatan) VALUES
-('22001', 'Ahmad Fauzan', 'Sains Data', 2022),
-('22002', 'Nisa Rahma', 'Sains Data', 2022);
+('020', 'ANINDYA SARASWATI AYU DEWAYANI', 'Sains Data', 2025),
+('204', 'ALIA NURUL DZIHNI', 'Sains Data', 2025),
+('200', 'LUTFI MUHAMMAD IRFANSYACH', 'Sains Data', 2025);
 
 INSERT INTO mata_kuliah (kode_mk, nama_mk, sks) VALUES
-('IF101', 'Pemrograman Dasar', 3),
-('IF102', 'Basis Data', 3),
-('IF103', 'Aljabar Matriks', 3);
+('IF101', 'Literasi Digital', 2),
+('IF102', 'Matematika Dasar', 3),
+('IF103', 'Pemrograman Dasar', 3),
+('IF104', 'Arsitektur Komputer dan Sistem Operasi', 3),
+('IF105', 'Pancasila', 2),
+('IF106', 'Matematika Diskrit', 3),
+('IF107', 'Aljabar Matriks', 3);
 
 INSERT INTO krs (nim, kode_mk, nilai, semester) VALUES
-('22001', 'IF101', 'A', 1),
-('22001', 'IF102', 'B+', 1),
-('22001', 'IF103', 'B', 1),
-
-('22002', 'IF101', 'A', 1),
-('22002', 'IF102', 'A', 1),
-('22002', 'IF103', 'B+', 1);
+-- Data untuk ANINDYA SARASWATI AYU DEWAYANI (020)
+('020', 'IF101', 'A', 1),
+('020', 'IF102', 'B+', 1),
+('020', 'IF103', 'B', 1),
+('020', 'IF105', 'A', 1),
+('020', 'IF106', 'C+', 2),
+('020', 'IF107', 'B', 2),
+-- Data untuk ALIA NURUL DZIHNI (204)
+('204', 'IF101', 'A', 1),
+('204', 'IF102', 'A', 1),
+('204', 'IF103', 'B+', 1),
+('204', 'IF104', 'B', 2),
+('204', 'IF105', 'A', 2),
+-- Data untuk LUTFI MUHAMMAD IRFANSYACH (200)
+('200', 'IF101', 'B+', 1),
+('200', 'IF102', 'C', 1),
+('200', 'IF104', 'B', 1),
+('200', 'IF106', 'B+', 2),
+('200', 'IF107', 'A', 2);
 
 INSERT INTO bobot_nilai VALUES
 ('A', 4.0),
